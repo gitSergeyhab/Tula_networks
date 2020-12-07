c71918c6676e2a827df27a1c616f77ba4b4d9c0e
Sergey <redmoloch@yanadex.ru>
2020-12-07 15:54:24 +0300

    subscribers.html, subscribers_all.html, one_subscriber.html   fixed

--- Tula_Networks/Tula_Networks/static/scripts/ph3.js
+++ Tula_Networks/Tula_Networks/static/scripts/ph3.js
@@ -1,7 +1,7 @@
 let btnAdds = document.querySelectorAll('.btn_add');
 let btnRemoves = document.querySelectorAll('.btn_remove');
 let phoneBar = document.querySelector('.phone_bar');
-
+//let listPhones = 'xz';
 
 function copyPhone(btn) {
     let bigParent = btn.parentElement.parentElement.parentElement;
@@ -14,21 +14,34 @@ function copyPhone(btn) {
     let btnDown = dublParent.querySelector('.btn_down');
     aRemove.classList.remove('display_none');
     aAdd.classList.add('display_none');
-    phoneBar.appendChild(dublParent);
-    return phoneBar;
+//    phoneBar.appendChild(dublParent);
+//    listPhones = phoneBar.children;
+//    console.log(listPhones.length, phoneBar.length);
+    return dublParent;
+}
+
+
+function removePhoneFromBar(btns) {
+    btns.forEach(item => {
+        item.addEventListener('click', function(evt) {
+            evt.preventDefault();
+            let removeFromBar = item.parentElement.parentElement.parentElement;
+            removeFromBar.remove();
+        })
+    })
 }
 
 
 btnAdds.forEach(item => {
     item.addEventListener('click', function(evt) {
         evt.preventDefault();
-        copyPhone(item);
+        let phoneForBar = copyPhone(item);
+        phoneBar.appendChild(phoneForBar)
         })
-
 })
 
-//console.log(phoneBar);
-console.log(phoneBar.children);
+//console.log(listPhones);
+//console.log(phoneBar.children);
 //console.log(phoneBar.parentElement);
-console.log(phoneBar.parentElement.children);
+//console.log(phoneBar.parentElement.children);
 //console.log(phoneBar.parentElement.children[2]);
