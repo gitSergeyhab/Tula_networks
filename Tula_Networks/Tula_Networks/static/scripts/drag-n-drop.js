2c9fffbc64901bb39e79936f2440b32176a3d943
Sergey <redmoloch@yanadex.ru>
2021-04-13 22:08:13 +0300

    drop person and drag phone

--- Tula_Networks/Tula_Networks/static/scripts/drag-n-drop.js
+++ Tula_Networks/Tula_Networks/static/scripts/drag-n-drop.js
@@ -1,6 +1,6 @@
 console.log('drag-n-drop.js');
 
-const templatePhoneRight = document.querySelector('#template-side-phone').content
+const templatePhoneRight = document.querySelector('#template-right-phone').content
 
 const rightBar = document.querySelector('.right_phone_bar');
 const rightBarContainer = rightBar.querySelector('.right-phone-container')
@@ -14,6 +14,8 @@ const dragStart = function() {
 }
 
 const dragEnd = function() {
+    // console.log('this',this)
+    // console.log('this.querySelector',this.querySelector('b'))
     this.querySelector('b').classList.remove('drag-element')
 }
 
@@ -25,7 +27,6 @@ personsR.forEach(personR =>{
 
 // куда
 const dragOver = function(evt) {
-    // console.log('dragover')
     evt.preventDefault();
 
 }
@@ -45,14 +46,65 @@ const dragLeave = function() {
 }
 
 const drop = function() {
-    console.log('drop');
     plusPhoneR.classList.remove('bg-green', 'bg-red');
     const dragElement = document.querySelector('.drag-element');
-    rightBarContainer.appendChild(dragElement);
     dragElement.classList.remove('drag-element');
+
+    const rightElem = makeRightBlock(dragElement);
+    rightBarContainer.appendChild(rightElem);
 }
 
 plusPhoneR.addEventListener('dragover', dragOver);
 plusPhoneR.addEventListener('dragenter', dragEnter);
 plusPhoneR.addEventListener('dragleave', dragLeave);
 plusPhoneR.addEventListener('drop', drop);
+
+
+function makeRightBlock(dragElement) {
+    const rightElem = templatePhoneRight.cloneNode(true);
+    const personDiv = rightElem.querySelector('.person_div');
+    const personName = personDiv.querySelector('.person_add');
+    const personLink = personDiv.querySelector('a');
+    personLink.href = dragElement.dataset.href;
+    personName.textContent = dragElement.textContent;
+    return rightElem;
+}
+
+
+// right-tel-drop
+const phonesR = document.querySelectorAll('.phone-number');
+console.log(phonesR)
+
+
+const dragStartPhone = function() {
+    this.querySelector('span').classList.add('drag-phone');
+}
+
+const dragEndPhone = function() {
+    this.querySelector('span').classList.remove('drag-phone')
+}
+
+phonesR.forEach(phoneR => {
+    phoneR.addEventListener('dragstart', dragStartPhone);
+    phoneR.addEventListener('dragend', dragEndPhone);
+})
+
+
+const dragEnterPhone = function(evt) {
+    const dragPhone = document.querySelector('.drag-phone');
+    if (dragPhone && evt.target.classList.contains('right-tel-drop')) {
+        evt.target.classList.add('bg-blue');
+    }
+}
+
+const dragLeavePhone = function(evt) {
+    if (evt.target.classList.contains('right-tel-drop')) {
+        evt.target.classList.remove('bg-blue');
+    }
+}
+
+rightBarContainer.addEventListener('dragover', dragOver);
+rightBarContainer.addEventListener('dragenter', dragEnterPhone);
+rightBarContainer.addEventListener('dragleave', dragLeavePhone);
+// rightBarContainer.addEventListener('drop', dropPhone);
+
